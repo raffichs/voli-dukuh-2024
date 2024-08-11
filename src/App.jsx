c6f2d37d59d5dca819737e762e72fe62f253346f
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-08-11 15:29:18 +0700

    update bg color

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Groups from "./components/Groups";
 import Fixtures from "./components/Schedules";
 
 function App() {
-  return <div className="content-center p-3 text-[#ffffff] bg-[#0238E0] border-[8px] border-[#0000007d]">
+  return <div className="content-center p-3 text-[#ffffff] bg-[#0070b8] border-[8px] border-[#0000007d]">
     <h1 className="font-medium text-2xl text-center mb-3">VOLI <span className="font-extra">DUKUH 2024</span></h1>
     <BrowserRouter>
       <Groups />
